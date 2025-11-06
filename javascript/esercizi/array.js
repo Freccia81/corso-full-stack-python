96721861355dfe6e6e8ffd5dfb2fe380697f52a6
happybanana78 <leonardo.mazzoleni1@gmail.com>
2025-11-06 12:25:21 +0100

    lesson 5 part 3

--- javascript/esercizi/array.js
+++ javascript/esercizi/array.js
@@ -26,3 +26,28 @@ if (indiceVerde !== -1) {
 
 // Stampo la lista aggiornata
 console.log(colori);
+
+// esercizio 3
+/*
+data la seguente matrice const matrice = [
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]
+]; fare il console.log del numero centrale (5)
+*/
+
+const matrice = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9,],
+];
+
+if (matrice.length % 2 !== 0) {
+    const rigaCentrale = Math.ceil(matrice.length / 2) - 1; // questo è uguale a 1        
+
+    if (matrice[rigaCentrale].length % 2 !== 0) {
+        const numeroCentrale = Math.ceil(matrice[rigaCentrale].length / 2) - 1;
+    
+        console.log(matrice[rigaCentrale][numeroCentrale]);
+    }
+}
