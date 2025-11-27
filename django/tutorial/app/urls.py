4610a5a74e7c4151a63f0df25f90e0785773336c
happybanana78 <leonardo.mazzoleni1@gmail.com>
2025-11-27 15:25:27 +0100

    added user update and creation

--- django/tutorial/app/urls.py
+++ django/tutorial/app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("contattaci/", views.contact, name="contact"),
     path("posts/", views.posts, name="posts"),
+    path('users/create/', views.create_user, name="create-user"),
+    path('users/update/', views.update_user, name="update-user")
 ]
